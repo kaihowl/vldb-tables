9807691e28e28ce622eeffe02abbfe147e76c4b2
kaihowl <kai@hoewelmeyer.eu>
2014-03-17 17:54:03 +0100

    Scaler with projection

--- scaler/scale.sh
+++ scaler/scale.sh
@@ -9,22 +9,24 @@ then
 fi
 
 rm -rf tmp/ && mkdir tmp/
-rm -rf output/ && mkdir output/
+mkdir -p output/
+rm -rf output/*.tbl
 
 # copy & remove date dashes / copy files
-sed s/-//g input/vbap_base_project.txt > tmp/vbap_base_project.txt
-sed s/-//g input/vbak_base_project.txt > tmp/vbak_base_project.txt
+# project vbap to MANDT, VBELN, MATNR, NETWR, KWMENG
+sed s/-//g input/vbap_base.txt | cut -f1,2,4,44,49 > tmp/vbap_base_project.txt
+# project vbak to MANDT, VBELN, ERDAT, KUNNR
+sed s/-//g input/vbak_base.txt | cut -f1,2,3,48 > tmp/vbak_base_project.txt
 sed 's/-//g' input/kna1.txt | sed 's/\t/|/g' > tmp/kna1.tbl
 sed 's/-//g' input/makt.txt | sed 's/\t/|/g' > tmp/makt.tbl
 sed 's/-//g' input/mara.txt | sed 's/\t/|/g' > tmp/mara.tbl
-sed 's/-//g' input/adrc.txt | tr -d '|' | sed 's/\t/|/g' > tmp/adrc.tbl 
+sed 's/-//g' input/adrc.txt | tr -d '|' | sed 's/\t/|/g' > tmp/adrc.tbl
 
-# MARA: MATNR
 python scaler.py $1
 
 # prepend schema info
-cp schema/vbap_base_project_schema.txt output/vbap.tbl
-cp schema/vbak_base_project_schema.txt output/vbak.tbl
+cut -d "|" -f1,2,4,44,49 schema/vbap_base_schema.txt > output/vbap.tbl
+cut -d "|" -f1,2,3,48 schema/vbak_base_schema.txt > output/vbak.tbl
 cp schema/kna1_schema.txt output/kna1.tbl
 cp schema/makt_schema.txt output/makt.tbl
 cp schema/mara_schema.txt output/mara.tbl
